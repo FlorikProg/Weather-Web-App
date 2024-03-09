1b30eeed5bbe0ac45a2710c57111aac2847cd4c4
Florik <132938276+FlorikProg@users.noreply.github.com>
2024-03-09 20:58:37 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        input_data = request.form['input_field_name']
+        # сохраняем введенные данные в переменную
+        print('Введенные данные:', input_data)
+
+
+        city = input_data  # Название города
+        api_key = "8038d45ae08cd330a4da56cdd51f5bee"  # Ваш API ключ OpenWeatherMap
+
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+        response = requests.get(url)
+        data = response.json()
+        weather_description = data["weather"][0]["description"]
+        temperature = data["main"]["temp"]
+
+        return render_template('html.html', temp=temperature)
+    
+    return render_template('html.html')
+
+
+        
+
+if __name__ == ("__main__"):
+    app.run(host="0.0.0.0")
